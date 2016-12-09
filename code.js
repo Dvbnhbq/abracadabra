983aa4a045105177e72ecbc903bbbaf342b2b596
Dvbnhbq <dvbnhbq@gmail.com>
2016-12-09 16:40:52 +0300

    fix

--- code.js
+++ code.js
@@ -1,9 +1,10 @@
 function calc(e) {
     var e = this.utf8_encode(e)
-    
-    function utf8_encode(e) {
-    }
-
 }
+
+function utf8_encode(e) {
+}
+
+
 
 document.getElementById("hash").setAttribute("value",calc())
